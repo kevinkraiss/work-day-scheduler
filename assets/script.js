0a3c91ec07a550a92e6bd5a61eb96a3e92f941ac
Kevin Kraiss <kevinkraiss@gmail.com>
2022-10-03 21:41:34 -0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+var dateDisplayEl = $('#currentDay')
+var timeDisplayEl = $('#currentTime')
+var timeBlockEl = $('.time')
+
+var today = moment()
+var currentHour = moment().format('H')
+var beginDay = '01-01-2022 08:00:00 AM'
+
+var past
+var present
+var future
+
+// creates timeblock elements
+$('.time-block').clone().appendTo('.container')
+$('.time-block').clone().appendTo('.container')
+
+
+// color codes time blocks based on past present or future
+// console.log(currentHour)
+
+// saves event info to localstorage
+
+// displays date in header
+dateDisplayEl.text(today.format('MMMM Do YYYY'))
+
+// displays current time in header
+timeDisplayEl.text(today.format('hh:mm a'))
+
+timeBlockEl.text(moment(beginDay).format('H a'))
+
+// event listener for event entry save button
